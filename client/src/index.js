cb3cff4f50da3d56559879a83b5f0c83a8339f8f
Shane Dorsey <scdorsey22@gmail.com>
2023-03-01 16:10:37 -0800

    passowrd and dependency fixes

--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,29 @@
 // Import necessary modules
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store/store';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store/store";
 
 // Import the main app components
-import App1 from './redux/App1';
-
+import App1 from "./redux/App1";
 
 // Import function to report web vitals
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
 // Import BrowserRouter for routing
 import { BrowserRouter } from "react-router-dom";
-
 
 // Render the main app component inside the Provider component and pass in the Redux store
 // Wrap the BrowserRouter component around the main app component for routing
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-    <App1 />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App1 />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // Function to report web vitals
